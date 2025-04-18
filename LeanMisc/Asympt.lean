6ffe8e868fef6d41d5fe139d20905d2ef7cc81ae
jkr11 <113939131+jkr11@users.noreply.github.com>
2025-04-18 23:23:05 +0200

    backup

---
+++ LeanMisc/Asympt.lean
@@ -0,0 +1,8 @@
+import Mathlib.Data.Complex.Basic
+import Mathlib.RingTheory.Polynomial.Basic
+import Mathlib.Data.Complex.Exponential
+import Mathlib.Analysis.SpecialFunctions.Exp
+
+open Complex
+
+def ω : ℂ := Complex.exp (2 * Real.pi * Complex.I / 10)
